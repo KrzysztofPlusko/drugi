fda8027f5365bdc39e4cb9ce70cf5d9145f4cf36
KrzysztofPlusko <47918367+KrzysztofPlusko@users.noreply.github.com>
2020-12-14 23:01:19 +0100

    controller repo

--- src/main/java/pl/krzysiek/java/drugi/controllers/ProjectController.java
+++ src/main/java/pl/krzysiek/java/drugi/controllers/ProjectController.java
@@ -1,15 +1,20 @@
 package pl.krzysiek.java.drugi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.krzysiek.java.drugi.dao.IProjectRepository;
 import pl.krzysiek.java.drugi.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+
+    @Autowired
+    IProjectRepository iProRepo;
 
     @GetMapping("new")
     public String displayProjectForm(Model model) {
