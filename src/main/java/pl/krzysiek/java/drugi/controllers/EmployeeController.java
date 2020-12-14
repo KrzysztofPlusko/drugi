a7f0216eb7501ae505c61b40fef8764cd2a5d59b
KrzysztofPlusko <47918367+KrzysztofPlusko@users.noreply.github.com>
2020-12-15 00:40:45 +0100

    employee controller save 2

--- src/main/java/pl/krzysiek/java/drugi/controllers/EmployeeController.java
+++ src/main/java/pl/krzysiek/java/drugi/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.krzysiek.java.drugi.dao.IEmployeeRepository;
 import pl.krzysiek.java.drugi.entities.Employee;
@@ -15,7 +16,7 @@ public class EmployeeController {
     @Autowired
     IEmployeeRepository iEmpRepo;
 
-    @GetMapping("new")
+    @GetMapping("/new")
     public String displayEmployeeForm(Model model) {
 
         Employee anEmployee = new Employee();
@@ -23,7 +24,7 @@ public class EmployeeController {
         return "new-employee";
     }
 
-
+    @PostMapping("/save")
     public String registerEmployee(Employee employee, Model model) {
 
         iEmpRepo.save(employee);
