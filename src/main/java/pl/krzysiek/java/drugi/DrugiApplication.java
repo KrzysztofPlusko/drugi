6d1811b7eff29208bf69c3177997f0a5acb38384
KrzysztofPlusko <47918367+KrzysztofPlusko@users.noreply.github.com>
2020-12-14 22:08:09 +0100

    Initial commit

---
+++ src/main/java/pl/krzysiek/java/drugi/DrugiApplication.java
@@ -0,0 +1,13 @@
+package pl.krzysiek.java.drugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/krzysiek/java/drugi/DrugiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzysiek.java.drugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
